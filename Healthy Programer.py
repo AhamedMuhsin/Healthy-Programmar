2058d1584e9aa2e1a58dd4c294de78c7aa168467
AhamedMuhsin <89242774+AhamedMuhsin@users.noreply.github.com>
2021-09-01 09:46:45 +0530

    Add some comment

--- Healthy Programer.py
+++ Healthy Programer.py
@@ -1,8 +1,9 @@
-
+# importing module use in this program
 from pygame import mixer
 from datetime import datetime
 from time import time
 
+# music loop function to play music
 def musiconloop(file, stopper):
     mixer.init()
     mixer.music.load(file)
@@ -13,10 +14,12 @@ def musiconloop(file, stopper):
             mixer.music.stop()
             break
 
+# log function to write the things in file
 def log_now(msg):
     with open("mylogs.txt", "a") as f:
         f.write(f"{msg} {datetime.now()}\n")
 
+#  main functions in this program
 if __name__ == '__main__':
     # musiconloop("water.mp3", "stop")
     init_water = time()
@@ -27,18 +30,21 @@ if __name__ == '__main__':
     eyessecs = 45*60
 
     while True:
+#         water drink 
         if time() - init_water > watersecs:
             print("Water Drinking time. Enter 'drank' to stop the alarm.")
             musiconloop('Water Pour Into Half Full Sink.mp3', 'drank')
             init_water = time()
             log_now("Drank Water at")
 
+#             eye exersice
         if time() - init_eyes >eyessecs:
             print("Eye exercise time. Enter 'doneeyes' to stop the alarm.")
             musiconloop('Monster Alien Grunt Sleepy.mp3', 'doneeyes')
             init_eyes = time()
             log_now("Eyes Relaxed at")
 
+#             pyhsical exersice
         if time() - init_exercise > exsecs:
             print("Physical Activity Time. Enter 'donephy' to stop the alarm.")
             musiconloop('Breathing Echo space.mp3', 'donephy')
